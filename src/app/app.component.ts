e5f1fd3ab60f2cabd4a633976e6e65b3a615d60f
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2021-09-07 18:13:37 -0400

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { FetcherService } from './fetcher.service';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <router-outlet></router-outlet>
-  `
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
 })
 
-export class AppComponent {
+export class AppComponent  {
 
-  
+  wallets=[]
+  transactions=[]
+  btc = 0
+  eth = 0
+  anyLeft = true;
+
+  constructor(private fetcher:FetcherService) { 
+    //console.log(this.fetcher.getAllWallets().subscribe(res => console.log(res)))
+    //console.log(this.fetcher.getAllTrans().subscribe(res => console.log(res)))
+    this.fetcher.getAllWallets().subscribe(data => this.getDataWallets(data))
+    this.fetcher.getAllTrans().subscribe(data => this.getDataTrans(data))
+  }
+
+  getDataWallets(data){
+
+    this.wallets=Object.entries(data);
+    this.updateTotalMoney()
+  }
+
+  getDataTrans(data){
+
+    this.transactions=Object.entries(data);
+    this.transLeft()
+  }
+
+  printThis(){
+    console.log(this.wallets)
+  }
+
+  onMine(idTrans, from, to, quantity, moneyType){
+    var personFrom = this.wallets.filter(item => item[1]["wallet"] == from)
+    var personTo = this.wallets.filter(item => item[1]["wallet"] == to)
+    var newQuantPerFrom = personFrom[0][1][moneyType] - quantity
+    var newQuantPerTo = personTo[0][1][moneyType] + quantity
+
+    console.log(newQuantPerFrom)
+    console.log(newQuantPerTo)
+
+    if(moneyType === "btc"){
+      this.fetcher.mineBTC(personFrom[0][0],newQuantPerFrom).subscribe(res => console.log(res))
+      this.fetcher.mineBTC(personTo[0][0],newQuantPerTo).subscribe(res => console.log(res))
+    }else{
+      this.fetcher.mineETH(personFrom[0][0],newQuantPerFrom).subscribe(res => console.log(res))
+      this.fetcher.mineETH(personTo[0][0],newQuantPerTo).subscribe(res => console.log(res))
+    }
+
+    this.fetcher.delete(idTrans).subscribe(res=>console.log(res))
+    this.fetcher.getAllWallets().subscribe(data => this.getDataWallets(data))
+    this.fetcher.getAllTrans().subscribe(data => this.getDataTrans(data))
+
+
+    window.location.reload();
+    
+
+
+   // this.fetcher.mine(personFrom[0][0],newQuantPerFrom,moneyType)
+  }
+
+  updateTotalMoney(){
+    console.log(this.wallets)
+    this.eth = 0
+    this.btc = 0
+    for(var i in this.wallets){
+      this.eth = this.wallets[i][1]["eth"] + this.eth
+      this.btc = this.wallets[i][1]["btc"] + this.btc
+      console.log(this.btc)
+    }
+  }
+
+  transLeft():boolean{
+    return this.transactions.find(item => item[1]['mineType'] !== 'PoS' ||
+    item[1]['miner'] > 20) === undefined
+  }
 }
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,44 +1,39 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'transactions',
-  template: `
+  template:`
   <div [style.background]="mineType === 'PoW'? 'yellow' : 'green'"
-       style="float: left; margin: 10px; padding: 10px;">
-	  <p>Date: {{date}}</p>
-    <p>From: {{from}}</p>
-	  <p>To: {{to}}</p>
-	  <p>Quantity: {{quantity}}</p>
-    <p>MoneyType: {{moneyType | uppercase}}</p>
-    <p>MineType: {{mineType}}</p>
-    <p>Miner: {{miner}}</p>
-    <button *ngIf="mineType === 'PoS' else myVarElse" 
-            (click)="onActivated()"
-            [disabled]="miner >= 20">
-            mine
-    </button>
-    <ng-template #myVarElse>
-        <button (click)="onActivated()">mine
-        </button>
-    </ng-template>
-  </div>
-  `,
-})
-export class TransactionsComponent{
+  style="float: left; margin: 10px; padding: 10px;">
 
-  @Input() date: string;
-  @Input() from: string;
-  @Input() to: string;
-  @Input() quantity:number;
-  @Input() moneyType: string;
-  @Input() mineType: string;
-  @Input() miner:number;
-  @Output() startTransaction = new EventEmitter();
+<p>Date: {{date}}</p>
+<p>From: {{from}}</p>
+<p>To: {{to}}</p>
+<p>Quantity: {{quantity}}</p>
+<p>MoneyType: {{moneyType | uppercase}}</p>
+<p>MineType: {{mineType}}</p>
+<p>Miner: {{miner}}</p>
+
+<ng-content></ng-content>
+
+
+</div>
+
+`,
+})
+export class TransactionsComponent implements OnInit {
+  @Input() id;
+  @Input() date;
+  @Input() from;
+  @Input() to;
+  @Input() quantity;
+  @Input() moneyType;
+  @Input() mineType;
+  @Input() miner;
 
   constructor() { }
 
-  onActivated() {
-    this.startTransaction.emit();
+  ngOnInit() {
   }
 
 }
