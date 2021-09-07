209cecbd08d98851a29c446ba5b2291b8fbf48c9
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2021-09-07 19:35:03 -0400

    testingAlmostDone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { WalletsComponent } from './wallets/wallets.component';
 import { TransactionsComponent } from './transactions/transactions.component';
+import { StrToDatePipe } from './str-to-date.pipe';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,HttpClientModule ],
-  declarations: [ AppComponent, WalletsComponent, TransactionsComponent ],
+  declarations: [ AppComponent, WalletsComponent, TransactionsComponent, StrToDatePipe ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
