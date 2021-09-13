06da71d103d33fe1decf27d7f9c270b1a5d87770
fabochoco9825 <fabiansegurondoferrel@gmail.com>
2021-09-12 23:40:06 -0400

    fixing bugs

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -3,38 +3,37 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor,
-  HttpParams
+  HttpInterceptor, HttpParams
 } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { AuthService } from '../../login/services/auth.service';
-import { catchError } from 'rxjs/operators';
+import {Observable, throwError} from 'rxjs';
+import {AuthService} from "../../login/services/auth.service";
+import {catchError} from "rxjs/operators";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
-
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.authService.getToken();
 
-    if (token) {
-      let params = new HttpParams();
-      params = params.append('auth', token);
+    if(token){
+      //let params = new HttpParams();
+      //params.append('auth', 'asdasdasdas')
+      //params.append('dgdfg', 'asdasdasdas')
+      //params.append('aughkhjkhjkth', 'asdasdasdas')
+
+      //console.log('PARAMS', params)
 
       request = request.clone({
-        url: `${request.url}`,
-        params
+        url: `${request.url}?auth=${token}`
       });
     }
 
     return next.handle(request).pipe(
-      catchError((err: any) => {
-        console.log('ERROR', err);
-        return throwError('ERROR EXTRA');
+      catchError( (err:any) => {
+        console.log('ERROR', err)
+        return throwError('ERROR EXTRA')
       })
     );
   }
+
 }
